e47ca6efdbe1cc741851e39d63736fc9c3d1e992
arief2020 <msyaifullahalarief@gmail.com>
2023-09-24 11:10:43 +0700

    test lagi fn

--- main.go
+++ main.go
@@ -33,10 +33,10 @@ func main() {
 
     // programmatically set swagger info
     docs.SwaggerInfo.Title = "Swagger Example API"
-    docs.SwaggerInfo.Description = "This is a sample server Movie."
+    docs.SwaggerInfo.Description = "This is a sample server E-comerce."
     docs.SwaggerInfo.Version = "1.0"
-    docs.SwaggerInfo.Host = "localhost:8080"
-    docs.SwaggerInfo.Schemes = []string{"http", "https"}
+    // docs.SwaggerInfo.Host = "localhost:8080"
+    // docs.SwaggerInfo.Schemes = []string{"http", "https"}
 
     // database connection
     db := config.ConnectDataBase()
@@ -45,5 +45,7 @@ func main() {
 
     // router
     r := routes.SetupRouter(db)
-    r.Run("localhost:8080")
+    r.Run()
+
+    
 }
